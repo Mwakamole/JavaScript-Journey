a5906703ca24524665f6c865eab9d14cb6ffe413
Mwakamole <mwakamoledaniel@gmailcom>
2023-12-14 11:54:28 +0300

    Assignment Operators

---
+++ JavaScript Operators/Assignment Operators/Assignment Operators.js
@@ -0,0 +1,28 @@
+let x;
+x = 2;
+document.getElementById("value").innerHTML = x;
+let y = 5;
+y += 3;
+document.getElementById("additionAssignment").innerHTML = y;
+let message = "Hello";
+message += " World!";
+document.getElementById("message").innerHTML = message;
+let z;
+z = 10;
+z -= 5;
+document.getElementById("subtractionAssignment").innerHTML = z;
+let t;
+t=10;
+t*=5;
+document.getElementById("multiplicationAssignment").innerHTML = t;
+let r = 2;
+r **= 3;
+document.getElementById("exponentiationAssignment").innerHTML = r;
+let g = 10;
+g /= 2;
+document.getElementById("divisionAssignment").innerHTML = g; 
+let f = 10;
+f %= 3;
+document.getElementById("remainderAssignment").innerHTML = f;
+
+
