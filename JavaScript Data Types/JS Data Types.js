c179aaf2bd1380f2138ad6591e23962f0ac13eea
Daniel <mwakamoledaniel@gmail.com>
2024-01-11 05:28:56 +0300

    JavaScript Numbers

--- JavaScript Data Types/JS Data Types.js
+++ JavaScript Data Types/JS Data Types.js
@@ -1,1 +1,5 @@
 document.getElementById("string").innerHTML=("Hello World!!")
+let y = 123e5;
+document.getElementById("positiveexponential").innerHTML =  y;
+let z = 123e-5;
+document.getElementById("negativeexponential").innerHTML = z;
