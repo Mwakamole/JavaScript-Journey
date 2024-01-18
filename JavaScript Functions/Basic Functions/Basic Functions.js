43ac4168a879bc355e1b3451c7efffa5222dd7a0
Daniel <mwakamoledaniel@gmail.com>
2024-01-18 10:41:49 +0300

    Another Practical of JavaScript Basic Functions

--- JavaScript Functions/Basic Functions/Basic Functions.js
+++ JavaScript Functions/Basic Functions/Basic Functions.js
@@ -2,3 +2,13 @@ function greet(){
     document.getElementById("function").innerHTML= "Hello Mwakamole"
 }
 greet()
+let x;
+function math(a,b){
+    x = a + b;
+    document.getElementById('button').innerHTML = x;
+}
+math(2,8);
+function identity(fname, lname){
+    document.getElementById('identity').innerHTML = "People know me as "+fname+" "+lname+" " ;
+}
+identity("Daniel", "Mwakamole");
